d4215ccbacb35fe7f46153726f918fd5c0fe28f2
Ludovic Muller <ludovicmuller1@gmail.com>
2025-07-24 17:24:34 +0200

    configure redirection and create /no-data

---
+++ app/no-data/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Content from "@/content/no-data.mdx";
+
+export default function Page() {
+  return (
+    <div className="md-content text-justify">
+      <Content />
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["ts", "tsx", "mdx"],
   /* config options here */
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
