38020f4d5f1e955dabb0827c65ee202de6b342cb
Ludovic Muller <ludovicmuller1@gmail.com>
2025-07-24 17:48:36 +0200

    chore: be explicit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
   output: "export",
 };
 
-const withMDX = createMDX({});
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
 
 export default withMDX(nextConfig);
